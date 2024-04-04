434b76ab3f06a84b11c494f6966bbb11d9eeb8de
zadorim <zadorim@kkszki.hu>
2024-04-04 17:51:34 +0200

    App.js módosítása

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React, {BrowserRouter as Router, NavLink, Routes, Route} from 'react';
+import axios from 'axios';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink to={'/'} className={({isActive}) => "nav-link" + (isActive ? "active" : "")}>
+                <span className="nav-link">Ital választó</span>
+              </NavLink>
+              </li>
+              <li className="nav-item">
+              <NavLink to={'/uj-pizza'} className={({isActive}) => "nav-link" + (isActive ? "active" : "")}>
+                <span className="nav-link">Új ital</span>
+              </NavLink>
+              </li>
+          </ul>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" exact element={<ItalListPage />} />
+        <Route path="/ital/:italmId" exact element={<ItaSinglePage />} />
+        <Route path="/uj-ital" exact element={<ItalCreatePage />} />
+        <Route path="/mod-ital/:italId" exact element={<ItalModPage />} />
+        <Route path="/del-ital/:italId" exact element={<ItalDelPage />} />
+      </Routes>
+    </Router>
   );
 }
 
